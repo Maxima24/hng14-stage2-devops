adb5a0105fd89bb10bb836146d9acaa1c74a3a55
Steel Maxima <steelmaxima21@gmail.com>
2026-04-22 21:13:55 +0100

    feat: add gitignore

--- api/main.py
+++ api/main.py
@@ -5,8 +5,7 @@ import os
 
 app = FastAPI()
 
-r = redis.Redis(host="localhost", port=6379)
-
+r = redis.Redis(host=os.getenv("REDIS_HOST", "localhost"), port=6379)
 @app.post("/jobs")
 def create_job():
     job_id = str(uuid.uuid4())
@@ -20,3 +19,6 @@ def get_job(job_id: str):
     if not status:
         return {"error": "not found"}
     return {"job_id": job_id, "status": status.decode()}
+@app.get("/health")
+def health():
+    return {"status": "ok"}
--- frontend/app.js
+++ frontend/app.js
@@ -1,12 +1,34 @@
-const express = require('express');
-const axios = require('axios');
-const path = require('path');
+import express from "express"
+import axios from "axios"
+import path from "path"
+import * as dotenv from 'dotenv';
+import cors from "cors"
+dotenv.config();
+import { fileURLToPath } from 'url';
+
+
+
+const PORT = process.env.PORT || 3000;
+const API_URL = process.env.API_URL || 'http://api:8000';
+if(!API_URL){
+  console.log("API_URL WAS NOT PASSED")
+  console.log(API_URL)
+
+}
+if(!PORT){
+  console.log("PORT WAS NOT PASSED")
+  console.log(PORT)
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
-
-const API_URL = "http://localhost:8000";
-
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(cors({
+  origin:"*",
+  methods:["GET","POST"]
+}))
 
 app.post('/submit', async (req, res) => {
   try {
@@ -26,6 +48,6 @@ app.get('/status/:id', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Frontend running on port 3000');
 });
